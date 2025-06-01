5632e646a3b42a4459e32aec9070e1efec3d64c2
Duc Nguyen <minhduc140583@gmail.com>
2025-06-01 22:08:35 +0700

    Add syntax highlighter

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,26 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <meta name="theme-color" content="#008577" />
         <meta name="description" content="Web site created using create-react-app" />
         <link rel="apple-touch-icon" href="../public/logo192.png" />
+        <link href="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/themes/prism-tomorrow.min.css" rel="stylesheet" />
+        <script src="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/components/prism-core.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/components/prism-clike.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/components/prism-java.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/components/prism-javascript.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/components/prism-typescript.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/components/prism-go.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/components/prism-bash.min.js"></script>
+        <link href="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/plugins/line-numbers/prism-line-numbers.min.css" rel="stylesheet" />
+        <script src="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/plugins/line-numbers/prism-line-numbers.min.js"></script>
+        <style>
+          {`
+          pre {
+            border-radius: 10px;
+            overflow: auto;
+            padding: 1em;
+            margin-bottom: 2em;
+          }
+          `}
+        </style>
         <title>Next App</title>
       </head>
       <body id="sysBody" className="full-header top-menu">
